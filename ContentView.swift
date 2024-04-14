e31253a4922723877489678fa554f7c5ff46db28
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-04-14 14:43:56 +0800

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Friends
+//
+//  Created by Liko Setiawan on 14/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Liko Setiawan on 14/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
